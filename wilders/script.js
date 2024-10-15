b9c2aba33956dfdac165b7cd1148f0a6dd292142
marco rojouan <marcoprojouan@outlook.fr>
2024-10-15 11:38:17 +0200

    first commit

---
+++ wilders/script.js
@@ -0,0 +1,8 @@
+import chalk from "chalk";
+const log = console.log;
+
+log(
+  `Hello ${chalk.magentaBright(`Amandine`)} ${chalk.blueBright(
+    `Remi`
+  )} ${chalk.redBright(`Christine`)} ${chalk.yellowBright(`Ayoub`)}`
+);
